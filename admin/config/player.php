4b7334926fbf6ce427927e186e9edf111413f029
Mario Santagiuliana <mario@marionline.it>
2010-12-28 16:20:07 +0100

    Initial commit

---
+++ admin/config/player.php
@@ -0,0 +1,19 @@
+<?php
+$title="ITheora, I really broadcast myself"; 
+$function_manual_play=true;
+$function_info=true;
+$function_ts=true;
+$function_name=true;
+
+$function_share=true;
+$function_download=true;
+$function_fullscreen=true;
+$function_options=true;
+
+$function_error_but=true;
+$function_podcast=true;
+$function_alt_download=true;
+
+$blacklist = Array ( 0 => "" ); 
+$whitelist = Array ( 0 => "" ); 
+?>
